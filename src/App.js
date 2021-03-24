38cba7eb03cd2fe6d47a3d3292a968175b60a209
celestinecyr <celestinecyr@gmail.com>
2021-03-24 13:30:18 +0800

    basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {          //function called App that returns a block of html 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
