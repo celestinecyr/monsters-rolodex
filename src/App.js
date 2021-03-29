ec4e7643ebbec74dacdf57f703968c7cdcf11f72
celestinecyr <celestinecyr@gmail.com>
2021-03-29 17:59:19 +0800

    Lifecycle Method - componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +6,20 @@ class App extends Component {
     super();
 
     this.state={
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'mon1'
-        },
-        {
-          name: 'Dracular',
-          id: 'mon2'
-        },
-        {
-          name: 'Zombie',
-          id: 'mon3'
-        }
-      ]
+      monsters: []  //{name: 'Frankenstein', id: 'mon1'}, {name: 'Dracular',id: 'mon2'},{name: 'Zombie',id: 'mon3'}
+      //empty array ^, we don't wanna hard code this anymore. Now, we're just going to wait for our component to mount and then fetch all the
     };
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')                            //fetch returns us a promise
+    //.then(response => console.log(response)                                      //that promise gives us some response of the actual body of our response but we dont know what that looks like at first, so we consolelog first to see whats in the 'database'
+    .then(response => response.json())                                           //if we just type response, we wouldn't rly understand what's shown so we get the response back in json format which we want
+    //then, this ^ will get returned to us as a new promise which we will now (v) have as the body, aka the user array
+    //.then(users => console.log(users))
+    .then(users => this.setState({ monsters: users }));                           //now we call setState as we did before; but now we set monsters to user array
+    }
+
 
   render() {                                                  
     return(                                                  
@@ -30,24 +27,19 @@ class App extends Component {
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}> { monster.name } </h1>           //need unique key because react needs to know what element needs updating, if one of the element in the array has a value that is gna change
         ))}      
-        
-        {/* 
-          Map returns us the return of whatever function we passed to it iterated over every element in this array
-          Map takes the function where the first argument is going to be the actual element that this function is going to get called on
-          usually its a new array when we call map but react is smart enough to know that if we end up rendering out a bunch of html blocks
-          right in an array then it'll just display those elements.
-        */}
-
-        {/* Note:
-          A good rule of thumb as to when to use the key attribute you saw in the previous video, is this: Anytime you use the map() function inside of render, 
-          or you have a list of the same jsx elements one after another, they need a key attribute (and CRA will warn you about it if you miss it)
-
-          Appendix 
-         */}
       </div>
     )
   }
-
 }
-
 export default App;
+
+/* 
+  Map returns us the return of whatever function we passed to it iterated over every element in this array
+  Map takes the function where the first argument is going to be the actual element that this function is going to get called on
+  usually its a new array when we call map but react is smart enough to know that if we end up rendering out a bunch of html blocks
+  right in an array then it'll just display those elements.
+
+Note:
+  A good rule of thumb as to when to use the key attribute you saw in the previous video, is this: Anytime you use the map() function inside of render, 
+  or you have a list of the same jsx elements one after another, they need a key attribute (and CRA will warn you about it if you miss it)
+*/
