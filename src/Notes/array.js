6207fe0e7620eeeba1bd25e099061d83085de41f
celestinecyr <celestinecyr@gmail.com>
2021-03-29 13:36:29 +0800

    Array Methods

--- src/Notes/array.js
+++ src/Notes/array.js
@@ -1,3 +1,8 @@
+// Array methods - methods that you can call on any array that will perform some kind of action on that array or with that array
+//every array method takes in a different kinda argument
+
+const myArray = [1,2,3,4];
+
 map();
 
 filter();
